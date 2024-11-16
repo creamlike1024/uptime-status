37b1013030484ad5f075091d5a6322ba63ff86f5
IcedTangerine <icedtangerine@outlook.com>
2024-11-16 13:20:08 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,36 +1,37 @@
 // 配置
 window.Config = {
 
-  // 显示标题
-  SiteName: '云生站点监测',
+  // 站点名
+  SiteName: '小林家网络监控',
+
+  // 站点链接
+  SiteUrl: '/',
+
+  // UptimeRobot Api 域名
+  // 只需填写域名部分，默认为官网域名
+  // 因官网 API 时不时的会 CROS 报错，可填自定义反代域名
+  // 详见说明
+  ApiDomain: 'uptimestatus.vercel.app',
 
   // UptimeRobot Api Keys
-  // 支持 Monitor-Specific 和 Read-Only
+  // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
-    'ur1078385-b9802a0868414b8a975d5038',
+    'ur1546592-7aa349db2a2031ce3e6f7c7d',
   ],
 
-    // 日志天数
+  // 是否显示监测站点的链接
+  ShowLink: false,
+
+  // 日志天数
   // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
   // 不过时间不要设置太长，容易卡，接口请求也容易失败
   CountDays: 60,
 
-  // 是否显示检测站点的链接
-  ShowLink: true,
-
   // 导航栏菜单
   Navi: [
     {
-      text: '主页',
-      url: 'https://tjys.tk'
-    },
-    {
-      text: '博客',
-      url: 'https://blog.qikaile.tk'
-    },
-    {
       text: 'GitHub',
       url: 'https://github.com/qikaile/uptime-status'
-    },
-  ],
+    }
+  ]
 };
